b451315515ad3e929a8b5a52be77bcdd2b898887
Fuzzwah <fuzzwah@github.com>
2021-08-06 22:13:17 +1000

    Update templates_markdown.py

--- src/gh2md/templates_markdown.py
+++ src/gh2md/templates_markdown.py
@@ -1,26 +1,7 @@
-BASE = r"""Export of Github issues for [{repo_name}]({repo_url}).{datestring}
+BASE = r"""{issues}"""
 
-{issues}
-"""
+ISSUE = r"""'{state}', '{title}', '{labels}', '{date}'"""
 
-ISSUE = r"""# [\#{number}]({url}) `{state}`: {title}
-{labels}
-#### <img src="{avatar_url}" width="50">[{author}]({author_url}) opened issue at [{date}]({url}):
+COMMENT = r""""""
 
-{body}
-
-{comments}
-
-
--------------------------------------------------------------------------------
-"""
-
-COMMENT = r"""#### <img src="{avatar_url}" width="50">[{author}]({author_url}) commented at [{date}]({url}):
-
-{body}
-"""
-
-ISSUE_FILE_FOOTNOTE = r"""
-
-[Export of Github issue for [{repo_name}]({repo_url}).{datestring}]
-"""
+ISSUE_FILE_FOOTNOTE = r""""""
